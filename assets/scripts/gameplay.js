40a153a03b3f59201f45dae4f90437d6d2cb5d92
punzie <c_a_mcginty@yahoo.com>
2019-03-03 22:18:12 -0500

    Images
    - Add background, favicon, canister, and dice images
    - Add display.js file with functions to dynamically display dice images
    matching what was rolled
    - Add css to set image sizes, font, and buttons

--- assets/scripts/gameplay.js
+++ assets/scripts/gameplay.js
@@ -1,9 +1,5 @@
-'use strict'
-const store = require('./store.js')
-
-let playerOneTurn = false
-
-// can is empty until fillCan
+const display = require('./display.js')
+// can is empty until startGame
 const can = []
 
 // playerHand is empty until takeDice
@@ -117,19 +113,13 @@ const dieThirteen = {
   }
 }
 
-// on gamestart, make sure playerHand is empty
-// make it playerOne's turn
+// on gamestart, make sure can and playerHand are empty
+// put the 13 dice in the can
 const startGame = function () {
+  $('.notPlaying').hide()
+  $('.firstRoll').show()
+  $('.afterFirstRoll').hide()
   playerHand.length = 0
-  playerOneTurn = !playerOneTurn
-  console.log(playerOneTurn)
-  fillCan()
-}
-
-// make sure can is empty
-// then put the 13 dice in the can
-const fillCan = function () {
-  console.log(playerOneTurn)
   can.length = 0
   can.push(dieOne)
   can.push(dieTwo)
@@ -149,23 +139,17 @@ const fillCan = function () {
 
 let dieNumber
 
-// checks if you've rolled already this turn
-// starts rollDice function if it's your first roll of the turn
-const startTurn = function () {
-  if (brains === 0 && shots === 0 && feet === 0) {
-    rollDice()
-  }
-}
-
 // take 3 random dice out of the can and put them into the player's hand
 const takeDice = function () {
-  console.log(playerOneTurn)
   // pick a random die
   dieNumber = [Math.floor(Math.random() * can.length)]
+  console.log(dieNumber)
   // add the die to playerHand
   playerHand.push(can[dieNumber])
   // remove the die from can
   can.splice(dieNumber, 1)
+  console.log(can)
+  console.log(playerHand)
 }
 
 let brains = 0
@@ -174,7 +158,8 @@ let shots = 0
 
 // roll the 3 dice in player's hand
 const rollDice = function () {
-  console.log(playerOneTurn)
+  $('.firstRoll').hide()
+  $('.afterFirstRoll').show()
   // pull 3 dice from the can
   do {
     takeDice()
@@ -182,42 +167,55 @@ const rollDice = function () {
   // roll the first die and increase the value of the thing you rolled
   playerHand[2].roll()
   if (playerHand[2].roll() === 'brain') {
+    display.findDieImg(playerHand[2], 2, 'brain')
     brains += 1
     document.getElementById('brains').value = brains
     playerHand.pop()
   } else if (playerHand[2].roll() === 'shot') {
+    display.findDieImg(playerHand[2], 2, 'shot')
     shots += 1
     document.getElementById('shots').value = shots
     playerHand.pop()
   } else {
+    display.findDieImg(playerHand[2], 2, 'feet')
     feet += 1
     document.getElementById('feet').value = feet
   }
+  console.log(brains, shots, feet)
+  console.log(playerHand)
   // roll the second die and increase the value of the thing you rolled
   playerHand[1].roll()
   if (playerHand[1].roll() === 'brain') {
+    display.findDieImg(playerHand[1], 1, 'brain')
     brains += 1
     document.getElementById('brains').value = brains
     playerHand.splice(1, 1)
   } else if (playerHand[1].roll() === 'shot') {
+    display.findDieImg(playerHand[1], 1, 'shot')
     shots += 1
     document.getElementById('shots').value = shots
     playerHand.splice(1, 1)
   } else {
+    display.findDieImg(playerHand[1], 1, 'feet')
     feet += 1
     document.getElementById('feet').value = feet
   }
+  console.log(brains, shots, feet)
+  console.log(playerHand)
   // roll the third die and increase the value of the thing you rolled
   playerHand[0].roll()
   if (playerHand[0].roll() === 'brain') {
+    display.findDieImg(playerHand[0], 0, 'brain')
     brains += 1
     document.getElementById('brains').value = brains
     playerHand.splice(0, 1)
   } else if (playerHand[0].roll() === 'shot') {
+    display.findDieImg(playerHand[0], 0, 'shot')
     shots += 1
     document.getElementById('shots').value = shots
     playerHand.splice(0, 1)
   } else {
+    display.findDieImg(playerHand[0], 0, 'feet')
     feet += 1
     document.getElementById('feet').value = feet
   }
@@ -226,90 +224,28 @@ const rollDice = function () {
   checkWinLose()
 }
 
-// feet counter goes back to zero
 const resetFeet = function () {
   feet = 0
   document.getElementById('feet').value = feet
 }
 
-// can only roll again if you've already rolled
 const rollAgain = function () {
-  console.log(playerOneTurn)
   if (brains > 0 | shots > 0 | feet > 0) {
-    // feet counter goes back to zero
     resetFeet()
     rollDice()
   }
 }
 
-// check win/loss conditions
-// eating 13 brains wins the game
-// getting 3 shots in a turn ends your turn and forfeits your brains for the turn
 const checkWinLose = function () {
-  console.log(playerOneTurn)
-  if (store.playerOneBrains >= 13 || store.playerTwoBrains >= 13) {
+  if (brains >= 13) {
     alert('you ate so many brains! you win!')
-    gameOver()
-  } else if (document.getElementById('shots').value >= 3) {
+  } else if (shots >= 3) {
     alert('shotgunned! you lose all your brains this turn')
-    loseTurn()
   }
-}
-
-const resetCounters = function () {
-  brains = 0
-  document.getElementById('brains').value = brains
-  shots = 0
-  document.getElementById('shots').value = shots
-  feet = 0
-  document.getElementById('feet').value = feet
-}
-
-store.playerOneBrains = 0
-store.playerTwoBrains = 0
-
-// add brains collected this turn to player's total brains this game
-const storeBrains = function () {
-  console.log(playerOneTurn)
-  if (playerOneTurn === true) {
-    store.playerOneBrains += brains
-  } else if (playerOneTurn === false) {
-    store.playerTwoBrains += brains
-  } else {
-    console.log('error')
-    console.log(playerOneTurn)
-    console.log(store)
-  }
-  console.log(store)
-  checkWinLose()
-}
-
-const loseTurn = function () {
-  resetCounters()
-  fillCan()
-  playerOneTurn = !playerOneTurn
-}
-
-const endTurn = function () {
-  // save brains, add to player's total brains
-  storeBrains()
-  // reset current brain/feet/shots counters
-  resetCounters()
-  // change player
-  playerOneTurn = !playerOneTurn
-  // put all dice back in cup
-  fillCan()
-  // swap to other player's turn
-  // coming soon
-}
-
-const gameOver = function () {
-  // can't press any buttons except start game
 }
 
 module.exports = {
   startGame,
-  startTurn,
-  rollAgain,
-  endTurn
+  rollDice,
+  rollAgain
 }
